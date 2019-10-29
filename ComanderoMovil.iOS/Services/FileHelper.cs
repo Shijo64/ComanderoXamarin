67f9ac931f2332d7a6cd43fd82865642c51ecbdf
Arturo Calvo <>
2019-10-29 09:55:25 -0600

    Commit inicial

---
+++ ComanderoMovil.iOS/Services/FileHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using ComanderoMovil.iOS.Services;
+using ComanderoMovil.Services;
+using System.IO;
+
+[assembly:Dependency(typeof(FileHelper))]
+namespace ComanderoMovil.iOS.Services
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            var docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string libFolder = Path.Combine(docFolder, "Database");
+            if(!Directory.Exists(libFolder))
+            {
+                Directory.CreateDirectory(libFolder);
+            }
+
+            return Path.Combine(libFolder, filename);
+        }
+    }
+}
