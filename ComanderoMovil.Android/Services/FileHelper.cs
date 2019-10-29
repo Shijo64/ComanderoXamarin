2eea6fd1110438aef64f9f1c0a637e1658da3603
Arturo Calvo <>
2019-10-29 09:56:13 -0600

    Commit android

---
+++ ComanderoMovil.Android/Services/FileHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ComanderoMovil.Droid.Services;
+using ComanderoMovil.Services;
+using Xamarin.Forms;
+using System.IO;
+
+[assembly:Dependency(typeof(FileHelper))]
+namespace ComanderoMovil.Droid.Services
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            return Path.Combine(path, filename);
+        }
+    }
+}
